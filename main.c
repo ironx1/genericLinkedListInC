f79f5daa074e8d725575379f460a722714cba2ba
Kaan Yalcinkaya <a4kaan@gmail.com>
2023-03-19 15:17:05 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <LinkedList.h>
+int main(){
+    addInterface(5.0);
+    addInterface(5.1);
+    addInterface(12.33);
+    addInterface(33.0);
+    addInterface(99.0);
+    printInterface(double);
+}
